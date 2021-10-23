cd110b62fd9feb6da71feb1d7040d6fabcea2a2d
Michael Nascimento <michael.nsc@outlook.com>
2021-10-22 22:21:49 -0300

    Comit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const initial_state = { nome: "", sobrenome: "" };
 
 function App() {
+  const [lista, setLista] = useState([]);
+  const [formulario, setFormulario] = useState(initial_state);
+
+  function handleSubmit() {
+    setLista([...lista, formulario]);
+    setFormulario(initial_state);
+  }
+
+  function handleDragStart(evento, indice) {
+    evento.dataTransfer.setData("indice", indice);
+  }
+
+  function handleDragEnter(evento) {
+    evento.target.classList.add("hovered");
+  }
+
+  function handleDragLeave(evento) {
+    evento.target.classList.remove("hovered");
+  }
+
+  function handleDrop(evento) {
+    const indice = evento.dataTransfer.getData("indice");
+    const self = evento.target.id;
+
+    let copiaLista = [...lista];
+    const temp = copiaLista[indice];
+    copiaLista[indice] = copiaLista[self];
+    copiaLista[self] = temp;
+
+    setLista(copiaLista);
+    evento.target.classList.remove("hovered");
+  }
+
+  //  ----------------------------
+  // MODO HARDCORE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // ===================================================
+  function handleMudarTexto(evento) {
+    const { id, value } = evento.target;
+    setFormulario((prev) => ({ ...prev, [id]: value }));
+  }
+  // ===================================================
+  // MODO HARDCORE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="form-group">
+        <div className="input-group">
+          <label htmlFor="nome">Nome</label>
+          <input type="text" name="nome" id="nome" value={formulario.nome} onChange={(e) => handleMudarTexto(e)} />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="sobrenome">Sobrenome</label>
+          <input
+            autoComplete="off"
+            type="text"
+            name="sobrenome"
+            id="sobrenome"
+            value={formulario.sobrenome}
+            onChange={(e) => handleMudarTexto(e)}
+          />
+        </div>
+        <button onClick={handleSubmit}>Mostrar</button>
+      </div>
+
+      <div className="drag-area">
+        {lista.map((item, index) => (
+          <div
+            key={index}
+            id={index}
+            className="list-item"
+            draggable={true}
+            onDrop={handleDrop}
+            onDragStart={(event) => handleDragStart(event, index)}
+            onDragLeave={handleDragLeave}
+            onDragOver={(event) => event.preventDefault()}
+            onDragEnter={handleDragEnter}
+          >
+            {item?.nome} {item?.sobrenome}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
